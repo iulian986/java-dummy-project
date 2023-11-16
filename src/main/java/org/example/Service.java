dffe69b81c9e81e9e213881e68d5130721ee1a15
AZOLTANS <anna.zoltanska@capgemini.com>
2023-11-16 23:47:00 +0100

    Generate refactored Client.java class

--- src/main/java/org/example/Service.java
+++ src/main/java/org/example/Service.java
@@ -1,7 +1,12 @@
+
 package org.example;
 
 public class Service {
-    public boolean isEven(int input) {
-        return input % 2 == 0;
+    
+    // Method to check if a given number is even
+    public boolean isEven(int number) {
+        
+        // Return true if the remainder of the number divided by 2 is 0
+        return number % 2 == 0;
     }
 }
