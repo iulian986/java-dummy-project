260016f699e4002c2cf75b67873f1f78f95f90ab
illia <ulyanov.iv@gmail.com>
2023-11-25 15:29:33 +0100

    [codeai] Generate unit tests, refactor classes

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,3 +1,4 @@
+
 package org.example;
 
 public class Client {
@@ -8,16 +9,15 @@ public class Client {
     }
 
     public String greeting(String name) {
+        validateName(name);
+        boolean evenLength = service.isEven(name.length());
+        String greeting = "Hello, %s".formatted(name);
+        return evenLength ? greeting.toUpperCase() : greeting;
+    }
+
+    private void validateName(String name) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("'name' must not be null or empty");
-        }
-
-        boolean even = service.isEven(name.length());
-        String greeting = "Hello, %s".formatted(name);
-        if (even) {
-            return greeting.toUpperCase();
-        } else {
-            return greeting;
         }
     }
 }
