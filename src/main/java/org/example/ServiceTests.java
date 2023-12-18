77d68f0aeaa6ca209b3e299b6ab1111b9d0eeba3
Marcin Jarecki <m.jarecki78@gmail.com>
2023-12-18 19:43:52 +0100

    [codeai] Generate unit tests, refactor classes

---
+++ src/main/java/org/example/ServiceTests.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class ServiceTests {
+
+    @InjectMocks
+    private Service service;
+
+    @Test
+    public void testIsEven_WhenInputIsEven_ReturnsTrue() {
+        // Arrange
+        int input = 4;
+
+        // Act
+        boolean result = service.isEven(input);
+
+        // Assert
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void testIsEven_WhenInputIsOdd_ReturnsFalse() {
+        // Arrange
+        int input = 5;
+
+        // Act
+        boolean result = service.isEven(input);
+
+        // Assert
+        assertThat(result).isFalse();
+    }
+}
