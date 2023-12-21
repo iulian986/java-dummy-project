dcbc9b21ab188165603116f79dc88edd91681565
bartmazu <bartmazu@LCE40618.corp.capgemini.com>
2023-12-21 16:27:49 +0100

    [codeai] Generate unit tests, refactor classes

---
+++ src/main/java/org/example/ServiceTests.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+public class ServiceTests {
+
+    @InjectMocks
+    private Service service;
+
+    @Test
+    public void testIsEven_WhenInputIsEven_ReturnsTrue() {
+        // Arrange
+        int input = 4;
+
+        // Act
+        boolean result = service.isEven(input);
+
+        // Assert
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void testIsEven_WhenInputIsOdd_ReturnsFalse() {
+        // Arrange
+        int input = 5;
+
+        // Act
+        boolean result = service.isEven(input);
+
+        // Assert
+        assertThat(result).isFalse();
+    }
+}
