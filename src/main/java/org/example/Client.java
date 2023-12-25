25443be0c60c436ff23b7801a4fd21a970517757
illia <ulyanov.iv@gmail.com>
2023-12-25 20:46:51 +0100

    [codeai] Generate unit tests, refactor classes

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -3,10 +3,20 @@ package org.example;
 public class Client {
     private final Service service;
 
+    /**
+     * Constructor for Client class
+     * @param service the service to be used
+     */
     public Client(Service service) {
         this.service = service;
     }
 
+    /**
+     * Generates a greeting message based on the length of the name
+     * @param name the name of the person to greet
+     * @return the greeting message
+     * @throws IllegalArgumentException if the name is null or empty
+     */
     public String greeting(String name) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("'name' must not be null or empty");
